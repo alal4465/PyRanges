f52d1d55b069f13e897c0c3e7ed9c08c5f5d4020
alal4465 <alonbenti@gmail.com>
2021-09-08 11:31:43 +0300

    Added concepts syntax sugar

--- PyRanges/PipeFunc.h
+++ PyRanges/PipeFunc.h
@@ -9,11 +9,11 @@ namespace PyRanges {
 	template <typename T>
 	concept PyIterableOrRange = std::derived_from<T, PyIterable<typename T::iterator>> || std::ranges::range<T>;
 
-	template <typename T>
-	concept ProcessFunction = std::derived_from<T, PipeFunc>;
+	template <typename T, typename TContainer>
+	concept ProcessFunction = std::derived_from<T, PipeFunc> && std::invocable<T, TContainer>;
 
 	struct PipeFunc {
-		template <PyIterableOrRange TContainer, ProcessFunction TPred> 
+		template <PyIterableOrRange TContainer, ProcessFunction<TContainer> TPred> 
 		requires std::invocable<TPred, TContainer>
 		friend constexpr inline auto operator| (const TContainer& container, TPred pred) {
 			return pred(container);
